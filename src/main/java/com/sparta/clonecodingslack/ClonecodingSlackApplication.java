93ba92d917f4a32319c9e177499a74d3e8b68dba
jihoon hyeon <97426920+hyeonjh@users.noreply.github.com>
2022-02-18 15:29:45 +0900

    first commit

---
+++ src/main/java/com/sparta/clonecodingslack/ClonecodingSlackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.clonecodingslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonecodingSlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClonecodingSlackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/clonecodingslack/ClonecodingSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.clonecodingslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonecodingSlackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
